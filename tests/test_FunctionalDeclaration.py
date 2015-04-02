9b382f39ca723ffdce24026742a40b8ab62fd63c
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2015-04-02 15:20:28 -0400

    Start of unit testing framework

---
+++ tests/test_FunctionalDeclaration.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+:Copyright: 2015 A. Lloyd Flanagan
+:Author: \\A. Lloyd Flanagan
+:Contact: a.lloyd.flanagan@gmail.com
+
+"""
+from unittest import TestCase
+
+from ..comp_css import FunctionalDeclaration
+
+
+class FunctionalDeclarationTestCase(TestCase):
+    """Unit tests for :class:`comp_css.FunctionalDeclaration`."""
+
+
+
+
+# Local Variables:
+# python-indent-offset: 4
+# fill-column: 100
+# indent-tabs-mode: nil
+# End:
